5687498a392d08f7dcd1c5aef87312fa77ba448b
Silvia Silpha Shetty D <133001106+Silvias03@users.noreply.github.com>
2023-12-01 22:00:40 +0700

    Add Routes

---
+++ routes/productsRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express'); //import express
+const router = express.Router();
+
+const productsController = require('../controllers/productsController');
+
+router.get('/products', productsController.getAllProducts);
+router.get('/products', productsController.getProductById);
+router.post('/products', productsController.createProduct);
+router.put('/products/:id', productsController.updateProduct);
+router.delete('/products/:id', productsController.deleteProduct);
+
+module.exports = router; // export to use in server.js
+
+
