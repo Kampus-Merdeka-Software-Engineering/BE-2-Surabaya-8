56f656603bcde3bb29cfe9d7dc57d7636a4b2414
Muhamad Iqbaal Dhoifulloh <iqbaaldhoifulloh@gmail.com>
2023-12-02 18:07:35 +0700

    coba upp node.module

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(productsRoutes)
 const response = require('./response.js')
 app.get('/', (req, res) => {
    const data = {
-    
+
    }
     response(200, data, "Request successful", res)
 })
@@ -20,6 +20,11 @@ app.get('/', (req, res) => {
 //     res.json(category);
 // } )
 
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+  });
+  
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Click this link to check It : http://localhost:${PORT}`)
