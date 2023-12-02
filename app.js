432a66bc0bad58ed330d78b887d2444529257f88
Muhamad Iqbaal Dhoifulloh <iqbaaldhoifulloh@gmail.com>
2023-12-02 17:45:49 +0700

    Add response.js

--- app.js
+++ app.js
@@ -7,11 +7,20 @@ const productsRoutes = require('./routes/productsRoutes')
 
 app.use(productsRoutes)
 
+const response = require('./response.js')
+app.get('/', (req, res) => {
+   const data = {
+    
+   }
+    response(200, data, "Request successful", res)
+})
+
+
 // app.get('/category', (req, res) =>{
 //     res.json(category);
 // } )
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`)
+    console.log(`Click this link to check It : http://localhost:${PORT}`)
 })
