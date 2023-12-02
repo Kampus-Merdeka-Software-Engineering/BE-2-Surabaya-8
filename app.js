d152dedce92c5fa5d3a9cda9f210d3459be80abf
Muhamad Iqbaal Dhoifulloh <iqbaaldhoifulloh@gmail.com>
2023-12-02 19:11:28 +0700

    udah ah pusing

--- app.js
+++ app.js
@@ -3,28 +3,24 @@ const express = require('express');
 
 const app = express();
 
-const productsRoutes = require('./routes/productsRoutes')
-
-app.use(productsRoutes)
-
 const response = require('./response.js')
 app.get('/', (req, res) => {
-   const data = {
+    const data = {
 
    }
     response(200, data, "Request successful", res)
 })
 
 
-// app.get('/category', (req, res) =>{
-//     res.json(category);
-// } )
+const productsRoutes = require('./routes/productsRoutes')
+
+app.use(productsRoutes)
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something went wrong!');
   });
-  
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Click this link to check It : http://localhost:${PORT}`)
