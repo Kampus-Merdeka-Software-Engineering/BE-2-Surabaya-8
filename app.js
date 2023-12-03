82ec495bef00fe838618bc04ad6665c77c21e7a9
Silvia Silpha Shetty D <133001106+Silvias03@users.noreply.github.com>
2023-12-03 08:00:37 +0700

    app.js
    update app.js

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
-// import library 
+require('dotenv').config();
 
 const express = require('express');
 const app = express();
 const port = 3000;
 const router = express.Router();
 
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
-const productsRoutes = require('./routes/productsRoutes');
+// const productsRoutes = require('./routes/productsRoutes');
 
 const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
+// const { products } = require('./config/prisma');
+
+app.use(express.json());
+
 const response = require('./response.js');
-const { products } = require('./config/prisma');
-
-app.use(productsRoutes);
-
-app.use(express.json())
-
 app.get('/', (req, res) => {
     const data = {
 
@@ -27,63 +25,77 @@ app.get('/', (req, res) => {
     response(200, data, "Request successful", res)
 })
 
-app.get('/customers', async (req, res) => {
-      const customers = await prisma.customers.findMany();
-      res.json(customers);
-    }
-);
 
-app.post('/products', async (req, res, next) => {
-    const newProductsData = req.body;
-
+app.post('/products', async (req, res) => {
     try {
+        const { id_category, title, price, location, description, benefits, thumbnail, images } = req.body;
+        
+        // Membuat produk baru dengan beberapa URL gambar
         const product = await prisma.products.create({
             data: {
-                id_category: newProductsData.id_category,
-                title: newProductsData.title,
-                price: newProductsData.price,
-                location: newProductsData.location,
-                description: newProductsData.description,
-                benefits: newProductsData.benefits,
-                thumbnail: newProductsData.thumbnail,
-                images: newProductsData.images
-            }
+             id_category,
+             title,
+             price,
+             location,
+            description,
+            benefits,
+            thumbnail,
+            images,
+            },
         });
 
-        console.log('Data customer baru ditambahkan:', products);
-        res.send("Create customer success");
-    } catch (error) {
-        console.error('Gagal menambahkan data customer baru:', error);
-        res.status(500).send('Gagal menambahkan data customer baru');
-    }
-});
+        res.status(201).json({
+            message: 'Product created successfully',
+            product,
+          });
+        } catch (error) {
+          console.error('Error creating product:', error);
+          res.status(500).json({
+            message: 'Internal Server Error',
+            error: error.message,
+          });
+        } 
+      });
       
 
-app.get('/customers', async (req, res) => {
-    const customers = await prisma.customers.findMany();
-    res.json(customers);
-  }
-);
+// app.get('/customers', async (req, res) => {
+//     const customer = await prisma.customers.findMany();
+//     res.json(customers);
+//   }
+// );
 
-app.post('/customers', async (req, res, next) => {
-    const newCustomerData = req.body;
-
+app.post('/customers', async (req, res) => {
     try {
+        const { nama, email, password } = req.body;
+
+        // Membuat customer baru dengan beberapa URL gambar
         const customer = await prisma.customers.create({
             data: {
-                nama: newCustomerData.nama,
-                email: newCustomerData.email,
-                password: newCustomerData.password
-            }
+              nama,
+              email,
+              password,
+            },
         });
 
-        console.log('Data customer baru ditambahkan:', customer);
-        res.send("Create customer success");
-    } catch (error) {
-        console.error('Gagal menambahkan data customer baru:', error);
-        res.status(500).send('Gagal menambahkan data customer baru');
-    }
-});
+        res.status(201).json({
+            message: 'Customer created successfully',
+            customer,
+          });
+        } catch (error) {
+          console.error('Error creating customer:', error);
+          res.status(500).json({
+            message: 'Internal Server Error',
+            error: error.message,
+          });
+        } 
+      });
+
+// app.use(productsRoutes)  
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+  });
 
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`)
