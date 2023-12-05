eb2fae5ef40d3762c17ced524dd92d753f0566dd
Muhamad Iqbaal Dhoifulloh <iqbaaldhoifulloh@gmail.com>
2023-12-05 20:23:59 +0700

    menuju pitching

--- app.js
+++ app.js
@@ -1,13 +1,84 @@
+const { PrismaClient } = require('@prisma/client');
 require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
-const { PrismaClient } = require('@prisma/client');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
 
 const prisma = new PrismaClient();
 const app = express();
+// middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: '54631',
+  resave: false,
+  saveUninitialized: false,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
+// LOGIN 
+passport.use(new LocalStrategy(
+  async (username, password, done) => {
+    try {
+      const user = await prisma.customers.findUnique({
+        where: {
+          username: username
+        }
+      });
+
+      console.log('Input Password:', password);
+      console.log('User:', user);
+
+      if (!user) {
+        console.log('Incorrect username.');
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+
+      const isPasswordValid = password === user.password;
+
+      console.log('Is password valid:', isPasswordValid);
+
+      if (!isPasswordValid) {
+        console.log('Incorrect password.');
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+
+      console.log('Login successful.');
+      // Mengirimkan pesan ke console dan menyimpan pesan serta username ke localStorage
+      done(null, { message: 'Login successful.', username: user.username });
+    } catch (err) {
+      console.error('Error in Passport LocalStrategy:', err);
+      return done(err);
+    }
+  }
+));
+
+app.post('/login', (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      console.error('Error during authentication:', err);
+      return res.status(500).json({ message: 'Internal Server Error' });
+    }
+
+    if (!user) {
+      console.log('Authentication failed:', info.message);
+      // Mengirimkan pesan ke console dan menyimpan pesan ke localStorage
+      return res.status(401).json({ message: info.message });
+    }
+
+    // Jika autentikasi berhasil
+    console.log('Authentication successful.');
+    // Mengirimkan pesan ke console dan menyimpan pesan serta username ke localStorage
+    res.status(200).json({ message: 'Login successful.', username: user.username });
+  })(req, res, next);
+});
+
+// PANGGIL RESPONSE
 const response = require('./response.js')
 app.get('/', (req, res) => {
     const data = {
@@ -108,11 +179,10 @@ app.post('/products', async (req, res) => {
     } 
   });
 
-// POST UNTUK CUSTOMERS
-app.post('/customers', async (req, res) => {
+// POST UNTUK SIGN IN a.k.a REGISTER
+app.post('/register', async (req, res) => {
   try {
     const { username, email, password } = req.body;
-
     const customer = await prisma.customers.create({
       data: {
         username,
@@ -135,6 +205,12 @@ app.post('/customers', async (req, res) => {
     });
   }
 });
+
+// LOGIN
+// app.post('/login', passport.authenticate('local'), (req, res) => {
+  // Jika autentikasi berhasil, pengguna akan diarahkan ke sini
+//   res.status(200).json({ message: 'Login successful' });
+// });
 
 // const productsRoutes = require('./routes/productsRoutes')
 
