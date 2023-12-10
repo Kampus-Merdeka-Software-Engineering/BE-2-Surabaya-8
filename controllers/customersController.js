90872276bfde99118b12868f5f9ad4ff3829b7d1
Muhamad Iqbaal Dhoifulloh <iqbaaldhoifulloh@gmail.com>
2023-12-10 12:40:16 +0700

    Update customersController.js

--- controllers/customersController.js
+++ controllers/customersController.js
@@ -1,6 +1,4 @@
 const prisma = require('../config/prisma');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
 const customersService = require('../services/customersService');
 
 // MENDAPATKAN SEMUA DATA CUSTOMERS
@@ -9,64 +7,44 @@ async function getAllCustomers(req, res) {
   res.json(customers);
 }
 
+// VALIDASI UNTUK LOGIN
+async function loginCustomers(req, res) {
+  const { username, password } = req.body;
+
+  try {
+    const user = await prisma.customers.findMany({
+      where: {
+        username: username,
+      }
+    });
+
+    console.log('Input Password:', password);
+    console.log('User:', user);
+
+    if (!user || user.length === 0) {
+      console.log('Incorrect username.');
+      return res.status(401).json({ message: 'Incorrect username.' });
+    }
+
+    // Membandingkan password 
+    if (password !== user[0].password) {
+      console.log('Incorrect password.');
+      return res.status(401).json({ message: 'Incorrect password.' });
+    }
+
+    console.log('Login successful.');
+    res.status(200).json({ message: 'Login successful.', username: user[0].username });
+  } catch (err) {
+    console.error('Error during login:', err);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+}
+
 // MEMBUAT DATA CUSTOMERS
 async function createCustomers(req, res) {
   const customerData = req.body;
   const customers = await customersService.createCustomers(customerData);
   res.json(customers);
-}
-
-// LOGIN
-passport.use(new LocalStrategy(
-  async (username, password, done) => {
-    try {
-      const user = await prisma.customers.findMany({
-        where: {
-          username: username
-        }
-      });
-
-      console.log('Input Password:', password);
-      console.log('User:', user);
-
-      if (!user) {
-        console.log('Incorrect username.');
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-
-      const isPasswordValid = password === user.password;
-
-      console.log('Is password valid:', isPasswordValid);
-
-      if (!isPasswordValid) {
-        console.log('Incorrect password.');
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-
-      console.log('Login successful.');
-      done(null, { message: 'Login successful.', username: user.username });
-    } catch (err) {
-      console.error('Error in Passport LocalStrategy:', err);
-      return done(err);
-    }
-  }
-));
-
-async function loginCustomers(req, res, next) {
-  passport.authenticate('local', (err, user, info) => {
-    if (err) {
-      console.error('Error during authentication:', err);
-      return res.status(500).json({ message: 'Internal Server Error' });
-    }
-
-    if (!user) {
-      console.log('Authentication failed:', info.message);
-      return res.status(401).json({ message: info.message });
-    }
-
-    console.log('Authentication successful.');
-    res.status(200).json({ message: 'Login successful.', username: user.username });
-  })(req, res, next);
 }
 
 module.exports = {
